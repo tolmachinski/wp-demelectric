d67068936ce63ed2d32e836a8f9825cbf7ece827
Bogdan <bogdan.podlesnii@gmail.com>
2022-11-10 09:35:10 +0200

    DEME-43: Allow connection with stage=false flag

--- web/dataTransfer/dataTransfer_config.php
+++ web/dataTransfer/dataTransfer_config.php
@@ -2,5 +2,6 @@
 
 define('MS3C_EXT_ROOT', realpath(dirname(dirname(__FILE__))));
 define('MS3COMMERCE_PRODUCTION_DB', 'ms3magento');
+define('MS3COMMERCE_STAGE_DB', 'ms3magento');
 
 require_once(MS3C_EXT_ROOT . '/../vendor/ms3commerce/dataTransfer/dataTransfer_config.php');
